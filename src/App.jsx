84dd6ec0aeaaa9e066dc12c29c2c8a8595c2d8e3
Michael <mikkelzuuu@gmail.com>
2022-03-08 20:45:12 +0100

    the actual code

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css';
+import List from './list';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div className='content'>
+       <List />
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/list.jsx
@@ -0,0 +1,40 @@
+import React, { useCallback } from 'react'
+import './list.css'
+
+
+// https://api.github.com/repos/mikkelzu/onlycodes/actions/runs/1953308688/jobs TODO: make singular jobs
+const List = () => {
+    const [data, setData] = React.useState([]);
+    const [dataIsLoaded, setDataIsLoaded] = React.useState(false);
+
+    const getItems = useCallback(() => {
+        fetch("https://api.github.com/repos/mikkelzu/onlycodes/actions/runs")
+        .then((res) => res.json())
+        .then((json) => {
+            setData(json.workflow_runs);
+            setDataIsLoaded(true);
+        }) ;
+    }, []);
+
+    React.useEffect(() => {
+         getItems()
+    }, [getItems]);
+
+    if (!dataIsLoaded) {
+        return <><h1>Loading...</h1></>
+    }
+
+        return (
+            <ul>
+                {
+                    data.map((item) => {
+                        return (<li className='list-item' key={item.id}> 
+                            {item.id} | {item.status} | {item.conclusion}
+                        </li>)
+                    })
+                }
+            </ul>
+        );
+}
+
+export default List;
